fb3f6ddb283c356b59b196f4552d7f07e382f281
quantumtom <cornyn@gmail.com>
2015-09-22 12:27:53 -0700

    A little reorganization.

--- app/include/js.php
+++ app/include/js.php
@@ -1,5 +1,4 @@
 <script type="application/javascript" src="/js/jquery/jquery-1.11.3.js"></script>
-<script type="application/javascript" src="/js/webkit/core.js"></script>
 <!--<script type="application/javascript" src="/js/fifteen.js"></script>-->
 <script type="application/javascript" src="/js/jquerymobile/jquery.mobile-1.4.5.js"></script>
 <script type="application/javascript">
