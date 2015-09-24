648759b32b66488b7039eb8e95f1a675384b1ebf
quantumtom <cornyn@gmail.com>
2015-09-23 21:44:02 -0700

    Changing Twitter feed to search query basis ("javascript") on twitter.php.
    Removing extraneous elements from contact form on talk.php.

--- app/lib/views/talk.php
+++ app/lib/views/talk.php
@@ -12,32 +12,10 @@
             <label for="telephone">Your phone number</label>
             <input type="tel" id="telephone" name="telephone" value="" placeholder="Your phone number"/>
         </div>
-        <div data-role="fieldcontain">
-            <fieldset data-role="controlgroup">
-                <label for="checkbox-1">Mobile Web</label>
-                <input type="checkbox" name="checkbox-1" id="checkbox-1" class="custom"/>
-                <label for="checkbox-2">Facebook Apps</label>
-                <input type="checkbox" name="checkbox-2" id="checkbox-2" class="custom"/>
-                <label for="checkbox-3">HTML5</label>
-                <input type="checkbox" name="checkbox-3" id="checkbox-3" class="custom"/>
-                <label for="checkbox-4">JavaScript</label>
-                <input type="checkbox" name="checkbox-4" id="checkbox-4" class="custom"/>
-                <label for="checkbox-5">CSS3</label>
-                <input type="checkbox" name="checkbox-5" id="checkbox-5" class="custom"/>
-                <label for="checkbox-6">WebKit Browsers</label>
-                <input type="checkbox" name="checkbox-6" id="checkbox-6" class="custom"/>
-            </fieldset>
-        </div>
     </form>
 </div>
 <div>
     <a href="mailto:info@socktan.com" data-role="button" data-inline="true"
-       data-mini="false">Email</a>
-</div>
-<div>
-    <a href="sms:1-424-242-9323" data-role="button" data-inline="true" data-mini="false">SMS</a>
-</div>
-<div>
-    <a href="tel:1-424-242-9323" data-role="button" data-inline="true" data-mini="false">Phone</a>
+       data-mini="false">Send</a>
 </div>
 
