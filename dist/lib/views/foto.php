51fa809ad39798055f9ca74775a2b10910a8d433
quantumtom <cornyn@gmail.com>
2015-09-24 10:49:00 -0700

    Renaming app ==> src.

---
+++ dist/lib/views/foto.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomascornyn
+ * Date: 9/23/15
+ * Time: 9:47 PM
+ */
---
+++ src/lib/views/reel.php
@@ -0,0 +1,66 @@
+<ul data-role="listview" data-inset="true">
+    <li>
+        <a target="_blank" href="http://noaa.socktan.com" title="Real-time satellite weather">
+            <img src="https://s3.amazonaws.com/socktan/img/noaa-96x96.png" class="portfolio" alt="Real-time satellite weather" />
+            <h3>Real-time satellite weather</h3>
+            <p>A fun little project animating a series of satellite weather images.</p>
+        </a>
+    </li>
+    <li>
+        <a target="_blank" href="http://connects.anthem.com" title="Usability redesign for Anthem/BlueCross/BlueShield">
+            <img src="https://s3.amazonaws.com/socktan/img/blue-cross.png" class="portfolio" alt="Usability redesign for Anthem/BlueCross/BlueShield" />
+            <h3>Anthem/BlueCross/BlueShield</h3>
+            <p>Anthem wanted to improve the usability of their four public outreach sites. After a third-party conducted a statistical usability study, we took the redesigned site and implemented all of the new design specs in a standards-compliant, cross-browser compatible web stack.</p>
+        </a>
+    </li>
+    <li>
+        <a target="_blank" href="http://www.webkitlab.com" title="jQuery Mobile Site for HTC ReZound with Beats Audio">
+
+            <img src="https://s3.amazonaws.com/socktan/img/htc-rezound-96x96.jpg" class="portfolio" alt="jQuery Mobile Site for HTC ReZound with Beats Audio" />
+            <h3>HTC ReZound with Beats Audio (mobile site)</h3>
+            <p>HTC introduced its Verizon ReZound handset and wanted a mobile site to show off the phone, on the phone. We customized a jQuery Mobile Framework site and threw in some cool extras. Like a 360 view of the phone that's rotated with the touch screen.</p>
+
+        </a>
+    </li>
+    <li>
+        <a target="_blank" href="http://www.gametrailers.com/netstorage/mobile" title="jQuery Mobile Site for GameTrailers.com">
+            <img src="https://s3.amazonaws.com/socktan/img/gt-mobile-reviews-96x96.png" class="portfolio" alt="jQuery Mobile Site for GameTrailers.com" />
+            <h3>GameTrailers (mobile site)</h3>
+            <p>MTV Networks Entertainment division wanted to spruce up their mobile presence ahead of the 2011 Electronics Entertainment Expo. Solution: rapid deployment of a jQuery Mobile Framework version of their site.</p>
+        </a>
+    </li>
+    <li>
+        <a target="_blank" href="https://www.facebook.com/bareescentuals?sk=app_185852401487880" data-rel="external" title="Asynchronous HTML5 Video Facebook App for Bare Escentuals Cosmetics - Be a Force of Beauty">
+            <img src="https://s3.amazonaws.com/socktan/img/be-96x96.jpg" class="portfolio" alt="Asynchronous HTML5 Video Facebook App for Bare Escentuals Cosmetics - Be a Force of Beauty" />
+            <h3>Asynchronous HTML5 Video Facebook App for Bare Escentuals Cosmetics, Be a Force of Beauty&trade;</h3>
+            <p>Bare Escentuals Cosmetics needed a Facebook app that would tie in with their Be a Force of Beauty&trade;. With seven video segments, it made
+                sense to build a video channel. We seized the opportunity to use the built-in video capabilities of the new browsers.</p>
+        </a>
+    </li>
+    <li>
+        <a target="_blank" href="http://www.spike.com/collection/bracketmaster" title="Captain Morgan Rum">
+            <img src="https://s3.amazonaws.com/socktan/img/captain-morgan-96x96.jpg" class="portfolio" alt="Captain Morgan Rum" />
+            <h3>Captain Morgan Bracket Master Challenge</h3>
+            <p>In previous years, the Captain Morgan Bracket Master Challenge had been built with Adobe Flash. We took that experience and recreated it in JavaScript, CSS, and HTML.</p>
+
+        </a>
+    </li>
+    <li>
+        <a target="_blank" href="http://www.kaboose.com" title="Disney Family Kaboose.com">
+            <img src="https://s3.amazonaws.com/socktan/img/disney-family-96x96.png" class="portfolio" alt="Disney Family Kaboose.com" />
+            <h3>Disney Family Kaboose.com</h3>
+            <p>Right around the time the iPad came out, people at Disney Family.com started to realize they were going to
+                have to do something about their Flash-driven content. We were able to reverse-engineer the "feature carousel"
+                that sits in the center well of the kaboose.com home page. The seamless transition was accomplished using
+                the YUI library.</p>
+        </a>
+
+    </li>
+    <li>
+        <a target="_blank" href="https://s3.amazonaws.com/socktan/images/portfolio/medium/zoobooks.jpg" title="Zoobooks">
+            <img src="https://s3.amazonaws.com/socktan/images/portfolio/small/zoobooks.jpg" class="portfolio" alt="Zoobooks" />
+            <h3>Zoobooks</h3>
+            <p>A monthly magazine for children, each issue of Zoobooks covers a different animal with pictures, educational diagrams and facts.</p>
+        </a>
+    </li>
+</ul>
