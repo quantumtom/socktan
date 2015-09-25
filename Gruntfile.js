12bb3aafc4b418c384f72726d920c229a2266b62
quantumtom <cornyn@gmail.com>
2015-09-25 14:15:29 -0700

    Updating Grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,99 @@
-/**
- * Created by thomascornyn on 9/24/15.
- */
+/* jshint undef: true, unused: true */
+
+(function () {
+
+    'use strict';
+
+    module.exports = function (grunt) {
+
+        /**
+         * Grunt Tasks and Configurations
+         */
+        grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+            copy: {
+
+            },
+            cssmin: {
+                options: {
+                    shorthandCompacting: false,
+                    roundingPrecision: -1
+                },
+                dev: {
+                    files: [{
+                        expand: true,
+                        cwd: 'src/css',
+                        src: ['style.css'],
+                        dest: 'dist/css'
+                    }]
+                }
+            },
+            jshint: {
+                options: {
+                    reporter: require('jshint-stylish')
+                },
+                test: [
+                    'src/js/*.js'
+                ]
+            },
+            clean: {
+                dist: {
+                    src: [
+                        'dist/*'
+                    ]
+                }
+            },
+            watch: {
+                dev: {
+                    files: [
+                        'src/*.html',
+                        'src/css/*.css',
+                        'src/js/*.js',
+                        'Gruntfile.js'
+                    ],
+                    tasks: [
+                        'copy:dev'
+                    ]
+                },
+                dist: {
+                    files: [
+                        'src/*'
+                    ],
+                    tasks: [
+                        'default'
+                    ]
+                },
+                js: {
+                    files: [
+                        'src/js/*.js'
+                    ],
+                    tasks: [
+                        'copy:js'
+                    ]
+                }
+            }
+        });
+
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
+        grunt.loadNpmTasks('grunt-contrib-htmlmin');
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+
+        /**
+         * Alias tasks
+         */
+
+        grunt.registerTask('default', [
+            'clean',
+            'cssmin'
+        ]);
+
+        grunt.registerTask('test', [
+            'jshint'
+        ]);
+
+    };
+
+}());
