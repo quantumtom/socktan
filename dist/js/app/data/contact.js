0275787fddb5e8333cef58c00aba0049e1dbf8e1
quantumtom <cornyn@gmail.com>
2016-06-17 07:55:16 -0700

    Remove form-horizontal class.

--- dist/js/app/data/contact.js
+++ dist/js/app/data/contact.js
@@ -2,7 +2,7 @@ define('data/contact', function () {
 
   var contactData = {
     title: 'contact us',
-    lead: '',
+    lead: 'Get in touch.',
     body: 'Whether you are a human or a robot, we would love to hear from you (robots are often more ' +
     'interesting). We can get started on your project right away.'
   };
