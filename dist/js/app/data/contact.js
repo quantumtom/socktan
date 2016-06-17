ba09829509818f0de1c0448fb3194f1edbe41d1f
quantumtom <cornyn@gmail.com>
2016-06-17 12:46:32 -0700

    More updates.

--- dist/js/app/data/contact.js
+++ dist/js/app/data/contact.js
@@ -1,7 +1,7 @@
 define('data/contact', function () {
 
   var contactData = {
-    title: 'contact socktan',
+    title: 'contact',
     lead: 'let\'s chat',
     body: 'Whether you are a human or a robot, we would love to hear from you (robots are often more ' +
     'interesting). We can get started on your project right away.'
