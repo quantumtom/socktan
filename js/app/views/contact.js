484a0ba8dc93be4488f46e6891ab2eed410275fd
quantumtom <cornyn@gmail.com>
2016-09-20 16:47:30 -0700

    Modify form id attribute.

--- js/app/views/contact.js
+++ js/app/views/contact.js
@@ -2,14 +2,41 @@ define([
     'views/base',
     'Handlebars',
     'hbar!parts/contact',
-    'json!data/contact.json'
-], function (BaseView, Handlebars, contactPart, contactData) {
+    'json!data/contact.json',
+    'models/contact'
+], function (BaseView, Handlebars, contactPart, contactData, ContactModel) {
     'use strict';
 
     return BaseView.extend({
-        initialize: function () {
+        events: {
+            'click #submitContact': 'onSubmit'
+        },
+        initialize: function (options) {
+            this.options = options;
             this.content = contactData;
             this.template = Handlebars.compile(contactPart(this.content));
+        },
+        onSubmit: function () {
+            var formData = $('#contactForm').serializeArray();
+            var contactModel = new ContactModel();
+
+            formData.forEach(function (c) {
+                var name = c.name;
+                var value = c.value;
+
+                contactModel.set(name, value);
+            });
+
+            this.model = contactModel;
+
+            this.model.save({
+                success: function (account) {
+                    window.console.log(JSON.stringify(account));
+                },
+                error: function (account, response) {
+                    window.console.error('error: ' + response);
+                }
+            });
         }
     });
 });
