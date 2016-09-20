bcd6fe19d67aa0a08a14e5dc3ad28bf4fcf900e2
quantumtom <cornyn@gmail.com>
2016-09-20 13:46:40 -0700

    Clean up loose ends.

--- js/app/models/contact.js
+++ js/app/models/contact.js
@@ -4,13 +4,11 @@ define([
     'use strict';
 
     return BaseModel.extend({
+        url: 'http://localhost:5000/api/v5/account',
         defaults: {
-            content: {
-                title: 'contact',
-                lead: 'let\'s chat',
-                body: 'Whether you are a human or a robot, we would love to hear from you (robots are often more ' +
-                'interesting). We can get started on your project right away.'
-            }
+            name: '',
+            email: '',
+            telephone: ''
         }
     });
 });
