d1d27fdc59ee9c12ee50b7c499095ac9c48272cf
quantumtom <cornyn@gmail.com>
2017-04-04 14:01:24 -0700

    Add mailer.

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var compress = require('compression');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 
 app.use(compress());
 
@@ -17,5 +18,34 @@ app.listen(app.get('port'), function() {
 );
 
 app.post('/user/contact', function (req, res) {
-    res.send('POST received.');
+    var mailOpts, smtpTrans;
+
+    //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+    smtpTrans = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: "cornyn@gmail.com",
+            pass: "vamsvsldztwsmjqr"
+        }
+    });
+
+    //Mail options
+    mailOpts = {
+        from: req.body.name + ' &lt;' + req.body.email + '&gt;', //grab form data from the request body object
+        to: 'cornyn@gmail.com',
+        subject: 'Website contact form',
+        text: req.body.message
+    };
+
+    smtpTrans.sendMail(mailOpts, function (error, response) {
+        //Email not sent
+        if (error) {
+            res.render('contact', { title: 'socktan.com - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' });
+        }
+        //Yay!! Email sent
+        else {
+            res.render('contact', { title: 'socktan.com - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' });
+        }
+    });
+
 });
